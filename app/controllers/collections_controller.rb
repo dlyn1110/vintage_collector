9965d7c77c240aed00ebcafdea1f046948dbcf35
github username <github email address>
2019-12-20 20:53:48 -0500

    added index and index,show and new view to collections view

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,16 +1,25 @@
 class CollectionsController < ApplicationController
 
     def index
+    	@collections = Collection.all
   	end
 
   	def new
   	end
 
   	def create
+  		@collection = current_user.collections.build(collection_params)
+  		
   	end
 
   	def destroy
   	end 
+
+  	private
+
+  	def collection_params
+  		params.require()
+  	end
 
 
 
